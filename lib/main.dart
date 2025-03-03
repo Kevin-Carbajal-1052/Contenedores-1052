d97031f924c55d1399bf664ebb59e769db361cdc
a.22308051281052 <a.22308051281052@cbtis128.edu.mx>
2025-03-03 18:15:21 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiContenedor());
+}
+
+class MiContenedor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Contenedor figuras \nKevin Carbajal Mat:1052', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
